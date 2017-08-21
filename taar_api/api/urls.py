c0044a6087185b15a0cedc19c2a22f6b873dbeaf
Mauro Doglio <mdoglio@mozilla.com>
2017-08-21 09:42:40 +0100

    Fix recommendations view url pattern

--- taar_api/api/urls.py
+++ taar_api/api/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^recommendations/(?P<uuid>[0-9a-f]{32})/$',
+    url(r'^recommendations/(?P<client_id>[0-9a-f]{32})/$',
         views.recommendations, name='recommendations'),
 ]
