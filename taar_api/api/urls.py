3a51351548e9e7861e79bcf79d4df6d9a919985f
Mauro Doglio <mdoglio@mozilla.com>
2017-08-30 15:30:39 +0100

    The recommender endpoint should accept UUIDs with hyphens

--- taar_api/api/urls.py
+++ taar_api/api/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^recommendations/(?P<client_id>[0-9a-f]{32})/$',
+    url(r'^recommendations/(?P<client_id>[0-9a-f-]{36})/$',
         views.recommendations, name='recommendations'),
 ]
