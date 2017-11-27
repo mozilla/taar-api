3f95de2ef5a3d177b16a5b65d42ba869ae80efb3
mlopatka <mlopatka@users.noreply.github.com>
2017-11-27 18:53:31 +0100

    Update urls.py

--- taar_api/urls.py
+++ taar_api/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.conf.urls import include, url
-from django.contrib import admin
+# from django.contrib import admin
 from django.views.static import serve as static_serve
 
 from .base import views
