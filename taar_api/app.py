6b0fe067a07d78ac1b5e7a6cc99ffc4e545bc060
Victor Ng <victor@crankycoder.com>
2018-04-09 23:46:32 +0200

    wired in taar-lite instead of taar

--- taar_api/app.py
+++ taar_api/app.py
@@ -3,13 +3,10 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from flask import Flask
-from flask import request
 from dockerflow.flask import Dockerflow
 
-from taar import recommenders
-from taar.context import default_context
-from taar.profile_fetcher import ProfileFetcher
-from taar import ProfileController
+from taar_lite.recommenders import GuidBasedRecommender
+from srgutil.context import default_context
 import json
 from decouple import config
 import optparse
@@ -19,8 +16,6 @@ dockerflow = Dockerflow(app)
 
 VALID_BRANCHES = set(['linear', 'ensemble', 'control'])
 
-DYNAMO_REGION = config('DYNAMO_REGION', default='us-west-2')
-DYNAMO_TABLE_NAME = config('DYNAMO_TABLE_NAME', default='taar_addon_data_20180206')
 TAAR_MAX_RESULTS = config('TAAR_MAX_RESULTS', default=10, cast=int)
 
 
@@ -38,54 +33,27 @@ class ResourceProxy(object):
 PROXY_MANAGER = ResourceProxy()
 
 
-@app.route('/api/recommendations/<uuid:uuid_client_id>/')
-def recommendations(uuid_client_id):
+@app.route('/taarlite/api/v1/addon_recommendations/<uuid:guid>/')
+def recommendations(guid):
     """Return a list of recommendations provided a telemetry client_id."""
     # Use the module global PROXY_MANAGER
     global PROXY_MANAGER
 
     # Coerce the uuid.UUID type into a string
-    client_id = str(uuid_client_id)
-
-    branch = request.args.get('branch', '')
-
-    if branch.endswith('-taar'):
-        branch = branch.replace("-taar", "")
-
-    if branch not in VALID_BRANCHES:
-        # Force branch to be a control branch if an invalid request
-        # comes in.
-        branch = 'control'
-
-    extra_data = {'branch': branch}
-
-    locale = request.args.get('locale', None)
-    if locale is not None:
-        extra_data['locale'] = locale
-
-    platform = request.args.get('platform', None)
-    if platform is not None:
-        extra_data['platform'] = platform
+    guid = str(guid)
 
     if PROXY_MANAGER.getResource() is None:
         ctx = default_context()
-        dynamo_client = ProfileController(region_name=DYNAMO_REGION,
-                                          table_name=DYNAMO_TABLE_NAME)
-        profile_fetcher = ProfileFetcher(dynamo_client)
-
-        ctx['profile_fetcher'] = profile_fetcher
 
         # Lock the context down after we've got basic bits installed
         root_ctx = ctx.child()
-        r_factory = recommenders.RecommenderFactory(root_ctx)
-        root_ctx['recommender_factory'] = r_factory
-        instance = recommenders.RecommendationManager(root_ctx.child())
+        instance = GuidBasedRecommender(root_ctx)
         PROXY_MANAGER.setResource(instance)
 
     instance = PROXY_MANAGER.getResource()
-    recommendations = instance.recommend(client_id=client_id,
-                                         limit=TAAR_MAX_RESULTS,
-                                         extra_data=extra_data)
+    cdict = {'guid': guid}
+    recommendations = instance.recommend(client_data=cdict,
+                                         limit=TAAR_MAX_RESULTS)
     # Strip out weights from TAAR results to maintain compatibility
     # with TAAR 1.0
     jdata = {"results": [x[0] for x in recommendations]}
