080c0b4ae3036e0c839bd05e8f66eb209a946461
Victor Ng <victor@crankycoder.com>
2018-05-23 10:50:09 -0400

    bumped dependency of taar-lite to 0.1.12 and set a new release 0.4.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='taar_api_lite',
-    version='0.4.2',
+    version='0.4.3',
     description=""" Flask webservice for deploying Mozilla taar-lite.
 This is a fork of the previous Django webservice implementation.""",
     author='Mozilla Foundation',
