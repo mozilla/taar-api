8bc122953fb21874a2885d11217f1298236503f2
Victor Ng <victor@crankycoder.com>
2018-05-24 15:37:15 -0400

    tagged 0.4.6 release with configurable cache timeouts

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='taar_api_lite',
-    version='0.4.5',
+    version='0.4.6',
     description=""" Flask webservice for deploying Mozilla taar-lite.
 This is a fork of the previous Django webservice implementation.""",
     author='Mozilla Foundation',
