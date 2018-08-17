d332a204f6103f100c3d5611d81fa14d4f137071
Victor Ng <victor@crankycoder.com>
2018-08-17 15:37:47 -0400

    taar-v3 launch draft version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='taar_api',
     description='This is https://github.com/mozilla/taar_api',
-    version='0.5.1',
+    version='0.5.2',
     author='Mozilla Corporation',
     author_email='vng@mozilla.com',
     url='https://github.com/mozilla/taar_api',
